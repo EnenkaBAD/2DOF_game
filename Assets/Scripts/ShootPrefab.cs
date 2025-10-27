15dc442d1998c593c8c59024203095002a00b627
EnenkaBAD <alexandrnazarevech08@gmail.com>
2025-10-27 22:38:43 +0300

    obs

--- Assets/Scripts/ShootPrefab.cs
+++ Assets/Scripts/ShootPrefab.cs
@@ -3,30 +3,18 @@ using UnityEngine;
 
 public class ShootPrefab : MonoBehaviour
 {
-    // Публичные переменные для настройки
-    public Transform shootPoint;    // Точка выстрела
-    public GameObject bulletPrefab; // Префаб пули/снаряда
-    public float speed = 10f;       // Скорость выстрела
-    public InputControllerReader inputControllerReader;
+    public Transform shootPoint;    
+    public GameObject bulletPrefab; 
+    public float speed = 10f;       
 
-    private float cooldown = 0f;    // Время перезарядки
-    private float fireRate = 0.2f;  // Интервал между выстрелами
 
 
     private void OnTriggerEnter(Collider other)
     {
-        // Проверяем, что это игрок и можно стрелять
-        if (other.CompareTag("Player") && cooldown <= 0)
+        if (other.CompareTag("Player"))
         {
             Shoot();
-            cooldown = fireRate; // Устанавливаем время перезарядки
         }
-    }
-    void Update()
-    {
-        // Уменьшаем время перезарядки
-        if (cooldown > 0)
-            cooldown -= Time.deltaTime;
     }
 
     void Shoot()
@@ -36,11 +24,7 @@ public class ShootPrefab : MonoBehaviour
 
         // Добавляем силу для движения
         Rigidbody rb = bullet.GetComponent<Rigidbody>();
-        if (rb != null)
-        {
-            // Для 3D
-            rb.AddForce(shootPoint.forward * speed, ForceMode.Impulse);
-        }
+        rb.AddForce(shootPoint.forward * speed, ForceMode.Impulse);
     }
 }
 
